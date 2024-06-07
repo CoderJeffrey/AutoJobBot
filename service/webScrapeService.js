88a47dcd1ef55d363855f7a66c81a964da03f9d5
Jeffrey Liu <jliu5021@usc.edu>
2024-06-06 23:08:27 -0700

    added parseDate and clear parsing of data

--- service/webScrapeService.js
+++ service/webScrapeService.js
@@ -1,7 +1,9 @@
 const { Builder, By, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
-(async function scrape() {
+const { DateStrToDateObj } = require('../helpers/time');
+
+async function scrape() {
     // Setup Chrome options
     let options = new chrome.Options();
     options.addArguments('--headless'); // Run Chrome in headless mode
@@ -18,13 +20,52 @@ const chrome = require('selenium-webdriver/chrome');
 
         // print the table body
         let tables = await driver.findElements(By.css('table'));
-        for (let i = 0; i < tables.length; i++) {
-            let table = tables[i];
-            let tableBody = await table.findElement(By.css('tbody'));
-            let tableBodyText = await tableBody.getText();
-            await console.log('Table Body:', tableBodyText);
+        let totalTables = tables.length;
+        console.log('Total Tables Length:', totalTables);
+
+        // table[0] is for the basic info of the repo (thus we start at 1)
+        let table = tables[1];
+        let tableBody = await table.findElement(By.css('tbody'));
+
+        // get the <tr> elements
+        let rows = await tableBody.findElements(By.css('tr'));
+        let totalRows = rows.length;
+        console.log('Total Rows Length:', totalRows);
+
+        for (let i = 0; i < 10; i++) {
+            console.log('Row:', i)
+            let row = rows[i];
+            let cells = await row.findElements(By.css('td'));
+            let totalCells = cells.length;
+            console.log('Total Cells Length:', totalCells);
+
+            let company = await cells[0].getText();
+            console.log('%d: Company: %s', i, company);
+
+            // get the job title and post date from the first row
+            let jobTitle = await cells[1].getText();
+            console.log('%d: Job Title: %s', i, jobTitle);
+
+            let jobLocation = await cells[2].getText();
+            console.log('%d: Job Location: %s', i, jobLocation);
+
+            let postDate = await cells[4].getText();
+            console.log('%d: Post Date: %s', i, postDate);
+
+            // convert the date to a Date object
+            let postDateObj = DateStrToDateObj(postDate);
+            console.log('%d: Post Date Object: %s', i, postDateObj);
+
+            // the date minus 1 day
+            let postDateMinusOneDay = new Date(postDateObj);
+            postDateMinusOneDay.setDate(postDateObj.getDate() - 1);
+            console.log('%d: Post Date Minus One Day: %s', i, postDateMinusOneDay);
         }
 
+        // print the table body
+        // let tableBodyText = await tableBody.getText();
+        // // print the top 1000 characters of the table body
+        // await console.log('Table Body:\n', tableBodyText.substring(0, 1000));
 
         // Locate the first row of the table
         // let firstRow = await driver.findElement(By.css('table tbody tr'));
@@ -43,4 +84,8 @@ const chrome = require('selenium-webdriver/chrome');
         // Quit the WebDriver
         await driver.quit();
     }
-})();
+}
+
+scrape();
+
+module.exports = { scrape };
